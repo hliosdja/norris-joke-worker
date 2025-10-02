8035b620ba1a94a0997432862eb09151920bd36a
hliosdja <harvey.centen09@gmail.com>
2025-10-02 15:52:02 +0800

    :construction: build: added koin work manager module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.koin.core)
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
+    implementation(libs.koin.androidx.workmanager)
 
     // ktor
     implementation(libs.ktor.client.core)
